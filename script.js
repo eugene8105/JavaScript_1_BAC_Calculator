87660772fe1f424a515e94ec73c9393c6f3446f3
Eugene Bobalo <ebobalo@ArgonInd.biz>
2020-10-27 14:16:44 -0500

    added some code

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+$(function(){
+    var val = o;
+    var numOfBeers = $("#numBeers").val();
+    alert(numOfBeers);
+})
+
+// for testing
+$(function() {
+    $("button").click(function() {
+        $("h2").toggleClass("green");
+    });
+});
